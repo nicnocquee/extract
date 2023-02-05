185d9a50958664a74e20d20f660f155955601737
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-02-05 18:49:56 +0100

    try other puppeter

--- pages/api/insta.ts
+++ pages/api/insta.ts
@@ -1,7 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as Yup from "yup";
-import puppeteer from "puppeteer";
+import edgeChromium from "chrome-aws-lambda";
+
+// Importing Puppeteer core as default otherwise
+// it won't function correctly with "launch()"
+import puppeteer from "puppeteer-core";
+
+// You may want to change this if you're developing
+// on a platform different from macOS.
+// See https://github.com/vercel/og-image for a more resilient
+// system-agnostic options for Puppeteeer.
+const LOCAL_CHROME_EXECUTABLE =
+  "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
 const key = process.env.API_KEY || "abcd";
 
@@ -22,7 +33,15 @@ export default async function handler(
     query: { url },
   } = await schema.validate(req);
 
-  const browser = await puppeteer.launch();
+  // Edge executable will return an empty string locally.
+  const executablePath =
+    (await edgeChromium.executablePath) || LOCAL_CHROME_EXECUTABLE;
+
+  const browser = await puppeteer.launch({
+    executablePath,
+    args: edgeChromium.args,
+    headless: false,
+  });
   const page = await browser.newPage();
 
   await page.goto(url);
@@ -50,6 +69,7 @@ export default async function handler(
     try {
       console.log(`Waiting for next button`);
       await page.$eval(`article button[aria-label="Next"]`, (button) =>
+        // @ts-ignore
         button.click()
       );
     } catch (error) {
